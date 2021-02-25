d3fbcb688374fede31344316cb80bd92684cd36d
youufis <youufis@sina.com>
2021-02-25 08:51:34 +0800

    使用系统默认字体

--- aitextword.py
+++ aitextword.py
@@ -143,7 +143,7 @@ def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     if (isinstance(img, np.ndarray)):  #判断是否OpenCV图片类型
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img)
-    fontText = ImageFont.truetype("font/font.ttf", textSize, encoding="utf-8")
+    fontText = ImageFont.truetype("simhei.ttf", textSize, encoding="utf-8")
     draw.text((left, top), text, textColor, font=fontText)
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
